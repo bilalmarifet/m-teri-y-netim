380df759827a51f6e2ec1e07241d68b6a6e06e88
oguz marifet <oguz.marifet@kobil.com>
2020-10-25 18:17:28 +0300

    confıg

--- src/screens/AppScreens/Home/index.tsx
+++ src/screens/AppScreens/Home/index.tsx
@@ -106,7 +106,7 @@ class CustomerHomeScreen extends Component<Props, State> {
   static navigationOptions = ({ navigation }) => {
 
     return {
-      title: 'Çubukçu Tic.',
+      title: 'Çubukcu Tic.',
 
       headerStyle: {
         backgroundColor: colors.headerColorTop,
