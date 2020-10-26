400b6cba6a370d57b122fa11ef07419c3f557b4a
oguz marifet <oguz.marifet@kobil.com>
2020-10-26 15:13:50 +0300

    config

--- src/services/AppConfig.ts
+++ src/services/AppConfig.ts
@@ -1,5 +1,5 @@
-export const BasestoreId = 2289;
-export const BaseStoreOwnerUserId = 4546;
+export const BasestoreId = 1180;
+export const BaseStoreOwnerUserId = 1223;
 
 export const BaseImage = require('../assets/BaseImage.png')
 export const BaseImageWithName = require('../assets/BaseImageWithName.png')
