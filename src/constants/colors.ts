66bd3271caa3092193ec3d179b61167c7d07e314
oguz marifet <oguz.marifet@kobil.com>
2020-12-12 12:30:03 +0300

    alinabilir -2

--- src/constants/colors.ts
+++ src/constants/colors.ts
@@ -9,7 +9,7 @@ export const colors = {
   headerColor: '#204051',
   textColorSecond: 'green',
   priceAndPlusColor : '#fe7171',
-  iconColor: '#2A5163',
+  iconColor: '#3A7585',
   iconColorSecond: '#204051',
   containerBgInner: '#fff',
   containerBgSecond: '#84a9ac',
