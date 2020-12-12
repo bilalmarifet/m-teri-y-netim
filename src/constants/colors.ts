cdb83759982f467b75237a3843b0266b0cc80d7e
oguz marifet <oguz.marifet@kobil.com>
2020-12-12 12:27:41 +0300

    will be cpick maybe - 1

--- src/constants/colors.ts
+++ src/constants/colors.ts
@@ -21,7 +21,8 @@ export const colors = {
   viewBackground: '#E5F1E5', // YESIL ARKA PLAN
   viewBackgroundText: '#56A6A4',
   textColorLighter: '#636363',
-  SignUpButtonColor: '#E1091F'
+  SignUpButtonColor: '#E1091F',
+  borderColorInner: '#193B52',
 };
 // export const colors = {
 //   primary: "#3F51B5",
