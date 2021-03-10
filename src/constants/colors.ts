beb4dcb5d49a9d6237fc907d88d7ac9f1002534f
oguz marifet <oguz.marifet@kobil.com>
2021-03-10 22:00:28 +0300

    main screen 3 lu product list

--- src/constants/colors.ts
+++ src/constants/colors.ts
@@ -21,8 +21,9 @@ export const colors = {
   viewBackground: '#E5F1E5', // YESIL ARKA PLAN
   viewBackgroundText: '#56A6A4',
   textColorLighter: '#636363',
-  borderColorInner: '#193B52',
+  splashScreenBackgroundColor : '#1A2535'
 };
+
 // export const colors = {
 //   primary: "#3F51B5",
 //   accent: "#AF2923",
